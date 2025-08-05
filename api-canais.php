71992fab57720231cf8ff1bf651db3004eb0c856
galaxyplay1234 <galaxyplay1234@gmail.com>
2025-08-05 04:27:51 -0300

    Update api-canais.php

--- api-canais.php
+++ api-canais.php
@@ -1,1 +1,37 @@
+<?php
+header('Content-Type: application/json');
+error_reporting(0);
 
+$url = 'https://sinalpublico.vercel.app/';
+$html = @file_get_contents($url);
+
+if (!$html) {
+    echo json_encode(['error' => 'Não foi possível carregar a página']);
+    exit;
+}
+
+$dom = new DOMDocument();
+libxml_use_internal_errors(true);
+$dom->loadHTML($html);
+libxml_clear_errors();
+
+$xpath = new DOMXPath($dom);
+$divs = $xpath->query('//div[contains(@class, "channel")]');
+
+$canais = [];
+
+foreach ($divs as $div) {
+    $dataUrl = $div->getAttribute('data-url');
+    $dataName = $div->getAttribute('data-name');
+    
+    $imgTag = $div->getElementsByTagName('img')->item(0);
+    $imgSrc = $imgTag ? $imgTag->getAttribute('src') : '';
+
+    $canais[] = [
+        'nome' => $dataName,
+        'logo' => $imgSrc,
+        'link' => $dataUrl
+    ];
+}
+
+echo json_encode($canais, JSON_UNESCAPED_UNICODE);
